b1c5c0d36041e879e53d6a9e4f982a1407298d88
Soleyda De Sousa <soleyda.desousa@lilab.pe>
2022-07-11 20:49:51 -0300

    calcular numeros primos

---
+++ index.js
@@ -0,0 +1,26 @@
+function obtenerPrimos(num) {
+  if (typeof num === "number" && Number.isInteger(num)) {
+    if (num >= 2) {
+      let auxiliar = [];
+      let primos = [];
+
+      for (let i = 2; i <= num; i++) {
+        if (!auxiliar[i]) {
+          primos.push(i);
+
+          for (let j = i << 1; j <= num; j += i) {
+            auxiliar[j] = true;
+          }
+        }
+      }
+
+      return primos;
+    } else {
+      return "El número debe ser mayor o igual a 2.";
+    }
+  } else {
+    return "El argumento debe ser un número entero.";
+  }
+}
+
+console.log(obtenerPrimos(10)); // [2, 3, 5, 7,]
